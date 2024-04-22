b7893c77fc07fa76c5bb518f6609c09a9e8f0d50
mateotiedra <mateo@tiedra.com>
2024-04-22 23:50:54 +0200

    basic testing version of image upload

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 import { HashLink, HashLink as RouterLink } from 'react-router-hash-link';
-import { Typography } from '@mui/material';
-
+import { Box, Typography } from '@mui/material';
 import Loading from '../Loading/Loading';
 
+import HomeLogic from './HomeLogic';
+
 function Home() {
+  const { saveFile, uploadFile, imgSrc } = HomeLogic();
   return (
     <>
       <Typography variant='h1' align='center'>
         Home
       </Typography>
+      <input type='file' onChange={saveFile} />
+      <button onClick={uploadFile}>Upload File</button>
+      <Box width='100%' component='img' src={imgSrc} />
     </>
   );
 }
--- src/pages/Home/HomeLogic.js
+++ src/pages/Home/HomeLogic.js
@@ -1,68 +1,40 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 import PageLogicHelper from '../../helpers/PageLogicHelper';
 
 const HomeLogic = () => {
   const { navigate, useLoadPage, API_ORIGIN, pageStatus, setPageStatus } =
     PageLogicHelper();
 
-  const [focusOnMap, setSetFocusOnMap] = useState(true);
-  const setFocusOnMap = (newFocusOnMap) => {
-    setSetFocusOnMap(newFocusOnMap);
-    if (pageStatus !== 'recent') setPageStatus('recent');
+  useLoadPage(async () => {});
+
+  const [file, setFile] = useState();
+  const [imgSrc, setImgSrc] = useState(
+    'https://storage.googleapis.com/bucket-quickstart-mori-lumm-421113/images%252F1713820714110'
+  );
+
+  const saveFile = (e) => {
+    setFile(e.target.files[0]);
   };
 
-  const [focusedStop, setFocusedStop] = useState(null);
-  const [reports, setReports] = useState();
-  const [mapCenter, setMapCenter] = useState([46.520308, 6.630665]);
-  const [stopsAround, setStopsAround] = useState([]);
-
-  useLoadPage(async () => {
-    await axios
-      .get(API_ORIGIN + '/latest')
-      .then(({ data }) => setReports(data.reports))
+  const uploadFile = () => {
+    const formData = new FormData();
+    formData.append('file', file);
+    axios
+      .post(API_ORIGIN + '/image', formData)
+      .then((res) => {
+        setImgSrc(res.data[0].linkUrl);
+        console.log(res.data[0]);
+      })
       .catch((err) => console.log(err));
-
-    setPageStatus('recent');
-  });
-
-  const selectFocusedStop = (stop) => {
-    setPageStatus('stop');
-    setFocusedStop(stop);
-  };
-
-  const newReport = () => {
-    setPageStatus('new-report');
   };
 
   return {
-    reports,
-    focusOnMap,
-    setFocusOnMap,
-    pageStatus,
-    focusedStop,
-    selectFocusedStop,
-    newReport,
-    mapCenter,
-    setMapCenter,
-    stopsAround,
-    setStopsAround,
+    saveFile,
+    uploadFile,
+    imgSrc,
   };
 };
-
-/*  // Fetch the best sellers
-    axios
-      .get(API_ORIGIN + '/book/best', { params: { limit: 3 } })
-      .then(({ data }) => setBestSellers(data))
-      .catch((err) => console.log(err)); */
-/*// Fetch the best school
-     axios
-      .get(API_ORIGIN + '/institution/best')
-      .then(({ data }) => setBestInstitutions(data))
-      .catch((err) => console.log(err)); */
-/* 
-  const goToSearch = () => {
-    navigate('/search');
-  }; */
 
 export default HomeLogic;
