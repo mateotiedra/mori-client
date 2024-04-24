be46e8607b077bbd54b8bb6c2cf7a82b24fce69d
mateotiedra <mateo@tiedra.com>
2024-04-24 11:07:50 +0200

    update config to be able to test on mobile

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -1,7 +1,10 @@
 const AppConfig = () => {
   const API_ORIGIN =
     process.env.NODE_ENV === 'development'
-      ? process.env.REACT_APP_API_URL_DEVELOPMENT //http://localhost:8000
+      ? window.location.href.replace(
+          '3000',
+          process.env.REACT_APP_API_PORT_DEVELOPMENT
+        ) //http://localhost:8000
       : process.env.REACT_APP_API_URL_PRODUCTION; //https://garegabot-ce14aee9f894.herokuapp.com
 
   return { API_ORIGIN };
