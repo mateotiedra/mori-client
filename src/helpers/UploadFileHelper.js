f9ac7e592ecff378b2783ba571e78fa492bd1f35
mateotiedra <mateo@tiedra.com>
2024-05-06 23:17:05 +0200

    specify timezone

--- src/helpers/UploadFileHelper.js
+++ src/helpers/UploadFileHelper.js
@@ -29,20 +29,7 @@ export const upload = (files, next) => {
 
   const now = new Date();
 
-  const formattedDate =
-    now.getFullYear() +
-    '-' +
-    (now.getMonth() + 1).toString().padStart(2, '0') +
-    '-' +
-    now.getDate().toString().padStart(2, '0') +
-    ' ' +
-    now.getHours().toString().padStart(2, '0') +
-    ':' +
-    now.getMinutes().toString().padStart(2, '0') +
-    ':' +
-    now.getSeconds().toString().padStart(2, '0');
-
-  formData.append('timestamp', formattedDate);
+  formData.append('timezone', Intl.DateTimeFormat().resolvedOptions().timeZone);
 
   axios
     .post(API_ORIGIN + '/image', formData, {
