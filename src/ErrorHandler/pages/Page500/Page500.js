17e5b58b368536c967ea48a852a3df3ae680051b
mateotiedra <mateo@tiedra.com>
2024-05-14 17:32:20 +0200

    make error page better

--- src/ErrorHandler/pages/Page500/Page500.js
+++ src/ErrorHandler/pages/Page500/Page500.js
@@ -1,11 +1,23 @@
 import React /* useContext */ from 'react';
 //import ErrorHandlerContext from '../../ErrorHandlerContext';
 
-import { /* Link, */ Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 function Page500() {
   //const { setErrorCode } = useContext(ErrorHandlerContext);
-  return <Typography>Site a crashé</Typography>;
+  return (
+    <Box
+      sx={{
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Typography>Oops une erreur est survenue. Réessaie plus tard.</Typography>
+    </Box>
+  );
 }
 
 export default Page500;
